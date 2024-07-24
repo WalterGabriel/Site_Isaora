db702a24c10118cd1df5bea38142424344d8a1fb
Walter G. Lima <57258582+WalterGabriel@users.noreply.github.com>
2024-07-24 11:36:54 +0200

    Mon Parcours Donne

--- js/slide-le-cabinet.js
+++ js/slide-le-cabinet.js
@@ -1,17 +1,3 @@
-const menuButton = document.querySelector('.header_menu-button');
-
-menuButton.addEventListener('click', openMenuList);
-
-function openMenuList() {
-  const menuList = document.querySelector('.header_nav--none');
-  menuList.classList.toggle('header_nav--active');
-  
-  menuButton.textContent === "menu"
-    ? menuButton.textContent = "close"
-    : menuButton.textContent = "menu"
-    ;
-}
-
 //CABINET SLIDE CONTROL
 let currentSlide = 0;
 const slides = document.querySelectorAll('.cabinet_slide');
@@ -76,5 +62,3 @@ function addSwipeListeners() {
         }
     }
 }
-
-
